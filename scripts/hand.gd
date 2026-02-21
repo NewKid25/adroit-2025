7f3917b8df9bcc90058c03ae3da8ff1e8d25498c
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2026-02-21 06:42:46 -0600

    task: redo folder structure

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -77,7 +77,7 @@ func cards_in_hand() -> int:
 	return get_child_count()
 
 func add_card(text: String):
-	var card: Node2D = preload("res://cadenuicode/card.tscn").instantiate()
+	var card: Node2D = preload("res://scenes/card.tscn").instantiate()
 	card.position.x = (cards_in_hand() * CARD_FULL_WIDTH) / 2.0
 	card.get_node("Label").text = text
 	add_child(card)
