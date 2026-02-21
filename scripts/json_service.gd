d0fd51dff19f7690d5de5cdc818b0b370e1350c4
NewKid25 <115599059+NewKid25@users.noreply.github.com>
2026-02-21 10:30:29 -0600

    feat : wrting json to file

--- scripts/json_service.gd
+++ scripts/json_service.gd
@@ -5,13 +5,15 @@ func _ready() -> void:
 	var dialog = Dialog.new()
 	dialog.text = "dialog"
 	dialog.nexts[Enums.CardPlayOutcome.ALL_SUCCESS] = "other dialog"
-	var dialogStr = JSON.stringify(JSON.from_native(dialog, true))
-	#dialogStr = JSON.from_native(dialog, true)
-#
+	
 	#dialogStr = JSON.stringify(dialog)
-	dialogStr = ForgeJSONGD.class_to_json_string(dialog)
+	# var dialogStr = ForgeJSONGD.class_to_json(dialog)
+	# ForgeJSONGD.store_json_file("res://data/test.json", dialogStr)
 	# dialogStr = JSON.stringify(JsonClassConverter.class_to_json(dialog, true))
 
-	var a_dia = ForgeJSONGD.json_string_to_class(Dialog, dialogStr)
-
-	print(dialogStr)
+	#var a_dia = ForgeJSONGD.json_string_to_class(Dialog, dialogStr)
+	var a_dia = ForgeJSONGD.json_file_to_class(Dialog, "res://data/test.json")
+	print(a_dia.nexts[Enums.CardPlayOutcome.ALL_SUCCESS])
+	
+	# print(dialogStr)
+	return
