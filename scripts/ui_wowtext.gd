3129c85949f46f08f55224030cad7bf4cf6bfb42
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2026-02-21 17:22:34 -0600

    feat: random rotations

--- scripts/ui_wowtext.gd
+++ scripts/ui_wowtext.gd
@@ -6,9 +6,9 @@ var rot_dir = 0.0
 func _ready() -> void:
 	visible = false
 
-	# rot_dir = randf_range(0.5, 1.5)
-	# if randi_range():
-	# 	rot_dir *= -1
+	rot_dir = randf_range(0.5, 2.0)
+	if randi_range(0, 1):
+		rot_dir *= -1
 
 
 func _process(delta: float) -> void:
@@ -16,6 +16,6 @@ func _process(delta: float) -> void:
 	time += delta * 2
 	var s = sin(time + 0.8)
 	scale = Vector2(s, s)
-	rotation = time * 0.1 - 0.03  * rot_dir
+	rotation += delta * 0.1 * rot_dir
 	if s <= 0:
 		queue_free()
