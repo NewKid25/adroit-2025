538afcf2d2120fe4f947cc4deed70690abbe5e30
ThatCreeper <creeperhostanimations@gmail.com>
2026-02-21 17:36:39 -0600

    do the thing

--- scripts/ui_atom.gd
+++ scripts/ui_atom.gd
@@ -1,4 +1,9 @@
 extends Sprite2D
 
+@export
+var inverted := false
+@export
+var speed_mod := 1.0
+
 func _process(delta: float) -> void:
-	rotation += delta * 0.07
+	rotation += delta * 0.07 * (-1 if inverted else 1) * speed_mod
