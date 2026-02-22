3a11d6d4502c0fa2878abe2a9aa3bf511adbe70d
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2026-02-22 12:19:41 -0600

    feat: music waits until after intro

--- scripts/intro_cutscene.gd
+++ scripts/intro_cutscene.gd
@@ -2,7 +2,7 @@ extends Node
 
 func next_scene():
 	get_tree().change_scene_to_file("res://scenes/title.tscn")
-
+	MusicPlayer.play()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("play_card") or Input.is_action_just_pressed("play_card_mouse"):
--- scripts/music_player.gd
+++ scripts/music_player.gd
@@ -3,4 +3,4 @@ extends AudioStreamPlayer
 func _ready():
 	bus = "Music"
 	stream = preload("res://assets/Entangled Love.ogg")
-	play()
+	# play()
