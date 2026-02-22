d1fc771f22000b00f8d584056e4c1402a5b52228
ThatCreeper <creeperhostanimations@gmail.com>
2026-02-22 11:07:46 -0600

    add mouse support to title

--- scripts/ui_title.gd
+++ scripts/ui_title.gd
@@ -8,6 +8,43 @@ var selected_menu_option := 0
 func _ready() -> void:
 	$WhiteOut.visible = true
 	$BlackOut.visible = true
+	
+	$CreditsButton/Button.pressed.connect(_on_credits_button_push)
+	$PlayButton/Button.pressed.connect(_on_play_button_push)
+	$CreditsButton/Button.mouse_entered.connect(_on_credits_button_enter)
+	$PlayButton/Button.mouse_entered.connect(_on_play_button_enter)
+
+func _on_credits_button_enter():
+	if fading_out:
+		return
+	if selected_menu_option == 1:
+		return
+	$AnimationPlayer.seek(1000, true)
+	$AnimationPlayer.play("select_credits")
+	SfxManager.play_sound(preload("res://assets/sfx/card_left.wav"))
+	selected_menu_option = 1
+
+func _on_play_button_enter():
+	if fading_out:
+		return
+	if selected_menu_option == 0:
+		return
+	$AnimationPlayer.seek(1000, true)
+	$AnimationPlayer.play("select_play")
+	SfxManager.play_sound(preload("res://assets/sfx/card_right.wav"))
+	selected_menu_option = 0
+
+func _on_credits_button_push():
+	if fading_out:
+		return
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+	SfxManager.play_sound(preload("res://assets/sfx/default_reaction.wav"))
+
+func _on_play_button_push():
+	if fading_out:
+		return
+	fading_out = true
+	SfxManager.play_sound(preload("res://assets/sfx/default_reaction.wav"))
 
 func _process(delta: float) -> void:
 	UIHelper.debug_fullscreen_toggle_key()
