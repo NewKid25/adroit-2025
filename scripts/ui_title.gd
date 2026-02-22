a4d56722890045b7eb02ddc4b1375911071689bd
ThatCreeper <creeperhostanimations@gmail.com>
2026-02-21 19:02:39 -0600

    works

--- scripts/ui_title.gd
+++ scripts/ui_title.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
-func _process(_delta: float) -> void:
+var fading_out := false
+var fade_time := 0.0
+
+func _process(delta: float) -> void:
 	UIHelper.debug_fullscreen_toggle_key()
 	
-	if Input.is_action_just_pressed("play_card"):
-		get_tree().change_scene_to_file("res://scenes/date_screen.tscn")
+	$WhiteOut.modulate.a -= delta
+	
+	if fading_out:
+		fade_time += delta
+		if fade_time >= 1.0:
+			get_tree().change_scene_to_file("res://scenes/date_screen.tscn")
+		$BlackOut.modulate.a = fade_time
+	else:
+		if Input.is_action_just_pressed("play_card"):
+			fading_out = true
