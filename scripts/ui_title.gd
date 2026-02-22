0c56e5bb1efda5e05c19c5d87a218815546040fe
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2026-02-22 02:34:40 -0600

    feat: title menu

--- scripts/ui_title.gd
+++ scripts/ui_title.gd
@@ -3,6 +3,8 @@ extends Node2D
 var fading_out := false
 var fade_time := 0.0
 
+var selected_menu_option := 0
+
 func _ready() -> void:
 	$WhiteOut.visible = true
 	$BlackOut.visible = true
@@ -17,6 +19,13 @@ func _process(delta: float) -> void:
 		if fade_time >= 1.0:
 			get_tree().change_scene_to_file("res://scenes/date_screen.tscn")
 		$BlackOut.modulate.a = fade_time
-	else:
+	elif not $AnimationPlayer.is_playing():
 		if Input.is_action_just_pressed("play_card"):
 			fading_out = true
+		elif Input.is_action_just_pressed("down") or Input.is_action_just_pressed("right"):
+			selected_menu_option = 1
+			$AnimationPlayer.play("select_credits")
+
+		elif Input.is_action_just_pressed("up") or Input.is_action_just_pressed("left"):
+			selected_menu_option = 0
+			$AnimationPlayer.play("select_play")
